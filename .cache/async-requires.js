da9c0eaafabc2e998bcb47d6179ed0f141eddce5
tai <asdffgap@gmail.com>
2020-10-17 20:35:37 -0700

    rm faq page

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,7 +3,6 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-faq-js": () => import("./../../../src/pages/faq.js" /* webpackChunkName: "component---src-pages-faq-js" */),
   "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,7 +6,6 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---src-pages-404-js": hot(preferDefault(require("/Users/taihunglu/Celer/celer-web/chainscale/src/pages/404.js"))),
-  "component---src-pages-faq-js": hot(preferDefault(require("/Users/taihunglu/Celer/celer-web/chainscale/src/pages/faq.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/taihunglu/Celer/celer-web/chainscale/src/pages/index.js")))
 }
 
